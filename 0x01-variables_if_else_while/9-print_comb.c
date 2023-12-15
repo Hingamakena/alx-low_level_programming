8a827e07e34da00d41d60d8d5b16bb600e59232c
hingamakena <hingamakena@gmail.com>
2023-12-15 06:17:43 -0800

    print the single digit combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: (0) Always success
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
