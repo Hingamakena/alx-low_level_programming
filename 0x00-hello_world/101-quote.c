db72ceaf7bd916921ae52e4e8315668943122e95
hingamakena <hingamakena@gmail.com>
2023-12-15 12:55:20 -0800

    prints to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,13 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	int i;
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (i = 0; str[i] != '\0'; i++)
+		putchar(str[i]);
+
+	putchar('\n');
+
 	return (1);
 }
