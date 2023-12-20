7110dab333537f4391f52f784e1bb93246678a1c
hingamakena <hingamakena@gmail.com>
2023-12-20 07:12:23 -0800

    prints the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @p: value to check the last digit
+ *
+ * Return: return last digit
+ */
+int print_last_digit(int p)
+{
+	int l = p % 10;
+
+	return (l);
+}
