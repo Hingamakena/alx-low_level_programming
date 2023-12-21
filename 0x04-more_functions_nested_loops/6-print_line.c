b64b84cff7e61e28ca2d90369e54ca92d865a997
hingamakena <hingamakena@gmail.com>
2023-12-21 11:47:35 -0800

    prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: length of the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0)
+		putchar('\n');
+	for (i = 0; i < n; i++)
+		putchar('_');
+
+	putchar('\n');
+}
