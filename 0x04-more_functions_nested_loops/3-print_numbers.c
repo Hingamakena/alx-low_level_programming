b8eb13326f6362a23d30e64430e52bc596a5e586
hingamakena <hingamakena@gmail.com>
2023-12-21 11:39:07 -0800

    print base ten numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print base ten numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char i = 48;
+
+	for (; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+}
