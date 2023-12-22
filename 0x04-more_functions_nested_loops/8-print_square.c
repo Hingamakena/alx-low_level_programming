19879f6f3ccd3698026cc7e303e47de794bdae13
hingamakena <hingamakena@gmail.com>
2023-12-21 18:50:18 -0800

    prints square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,17 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			putchar('#');
-		}
+	if (size <= 0)
 		putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
 	}
 }
