7616cb1ada7c94620de357d02da77836e6c7f822
hingamakena <hingamakena@gmail.com>
2023-12-21 18:24:38 -0800

    prints a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,10 +11,13 @@ void print_line(int n)
 	int i;
 
 	if (n == 0)
+	{
 		putchar('\n');
-
-	for (i = 0; i < n; i++)
-		putchar('_');
-
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+			putchar('_');
+	}
 	putchar('\n');
 }
