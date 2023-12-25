b3f2dfa8cc2a3d4f070ae1ae33348e9e39f78927
hingamakena <hingamakena@gmail.com>
2023-12-24 22:47:31 -0800

    prints 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,17 @@
  */
 void more_numbers(void)
 {
-	int j, k, i;
+	int j, i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 15; j++)
 		{
+			if (j > 9)
+			{
+				putchar(j / 10 + '0');
+			}
 			putchar(j % 10 + '0');
-		}
-		for (k = 10; k < 15; k++)
-		{
-			putchar(k / 10 + '0');
-			putchar(k % 10 + '0');
 		}
 		putchar('\n');
 	}
