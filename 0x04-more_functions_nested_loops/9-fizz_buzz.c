e233bf1fdfb1212d160eaabc01d9ecd2695e8fd4
hingamakena <hingamakena@gmail.com>
2023-12-31 01:43:30 -0800

    prints one to one humdred

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: (0) Always success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		printf("%d ", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
