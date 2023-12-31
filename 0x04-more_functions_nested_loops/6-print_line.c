f1a8286ac7e3b1dc6535edc97b6f7ddd9514fe38
hingamakena <hingamakena@gmail.com>
2023-12-31 01:25:36 -0800

    added the main prototype

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,12 @@
 void print_line(int n)
 {
 	int i;
-	
-	for (i = 1; i <= n; i++)
+	if (n > 0)
 	{
-		_putchar('_');
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	}
-	if (n <= 0)
-		_putchar('\n');
 	_putchar('\n');
 }
