8b642702b29315901fea13090976d6904093a1ba
hingamakena <hingamakena@gmail.com>
2023-12-31 01:36:08 -0800

    prints a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,14 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i;
 	int j;
 
-	for (; i < j; i++)
+	for (i = 0; i < j; i++)
 	{
-		for (; j < n+1; j++)
+		for (j = 0; j < n+1; j++)
 		{
-			putchar('$');
+			_putchar('\\');
 		}
 	}
 	putchar('\n');
