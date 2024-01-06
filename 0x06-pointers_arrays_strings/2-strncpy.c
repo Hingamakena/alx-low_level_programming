e8bb108a0561ac463ba07e1db66055c51a6d90f9
hingamakena <hingamakena@gmail.com>
2024-01-05 22:25:05 -0800

    added the betty and the header file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: destination
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
@@ -6,8 +16,6 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		src[i] = dest[i];
 	}
-	dest[i] = '\0';
-
 	dest[i] = '\0';
 
 	return (dest);
