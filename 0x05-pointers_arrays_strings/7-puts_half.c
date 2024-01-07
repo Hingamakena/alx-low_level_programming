f96b7324d8ad7accc5840a681670a70f959a98e1
hingamakena <hingamakena@gmail.com>
2024-01-07 04:19:23 -0800

    puts the second half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string
+ * @str: string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int half;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	if (i % 2 == 1)
+	{
+		i = i - 1;
+	}
+
+	half = i / 2;
+
+	for (i = half; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
