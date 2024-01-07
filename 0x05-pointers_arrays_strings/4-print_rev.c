cd8ebb0d82b87f6aebae9639d43b59caef723f9f
hingamakena <hingamakena@gmail.com>
2024-01-07 01:35:40 -0800

    prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,17 +9,14 @@
 void print_rev(char *s)
 {
 	int i;
-	int j;
-
-	j = i + 1;
 
 	for (i = 0; s[i] != '\0'; i++)
 		;
 
-	while (j > 0)
+	while (i > 0)
 	{
-		_putchar(s[j]);
-		j--;
+		i--;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
