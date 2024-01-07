6b6aafb56aba48844b164cd8bb04b7a882123f30
hingamakena <hingamakena@gmail.com>
2024-01-07 01:41:30 -0800

    reverses a string'

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	while (i > 0)
+	{
+		i--;
+	}
+}
