08504dba23b8fc7f2729572a8486debfca8dd08d
hingamakena <hingamakena@gmail.com>
2024-01-07 05:59:01 -0800

    prints the list of an integer array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; a[i] <= n; i++)
+	for (i = 0; a[i] < n; i++)
 	{
-		_putchar(a[i]);
+		printf("%d", a[i]);
 	}
-	_putchar('\n');
+	printf("\n");
 }
