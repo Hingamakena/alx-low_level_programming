c6cafb0142f43922fbd77a0fc8da2ddaf9258356
hingamakena <hingamakena@gmail.com>
2024-01-12 10:24:54 -0800

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arcument count
+ * @argv: arguments
+ *
+ * Return: (0)
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	if (argc != 3)
+		printf("Error\n");
+
+	return (0);
+}
