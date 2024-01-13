ac324b0590f9eba491e2d06ce551900a33771ad3
hingamakena <hingamakena@gmail.com>
2024-01-12 22:59:53 -0800

    multiplies two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -10,7 +11,12 @@
 int main(int argc, __attribute__((unused))char *argv[])
 {
 	if (argc != 3)
+	{
 		printf("Error\n");
-
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
 	return (0);
 }
