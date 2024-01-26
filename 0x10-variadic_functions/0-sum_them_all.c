46c5c19c89984d29e5a7c85a5125a1bb20b55691
hingamakena <hingamakena@gmail.com>
2024-01-26 00:55:49 -0800

    finds the sum of all variadic arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all arguments on the variadic function
+ * @n: the first argument
+ *
+ * Return: (sum)
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int count = 0;
+	va_list total;
+
+	va_start(total, n);
+
+	for (count = 0; count < n; count++)
+	{
+		sum += va_arg(total, int);
+	}
+
+	return (sum);
+}
