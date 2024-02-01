0aea9d36984621b72ee341246ea30449094b1489
hingamakena <hingamakena@gmail.com>
2024-01-31 23:37:35 -0800

    structure named dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct name
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: woner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
