7ebf8a570c918351945cff1f49ef5a7e7bd93292
hingamakena <hingamakena@gmail.com>
2024-02-06 09:16:08 -0800

    prints the length of the list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints the length of the list
+ * @h: name of the list
+ *
+ * Return:(0) success
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+	}
+
+	return (count);
+}
