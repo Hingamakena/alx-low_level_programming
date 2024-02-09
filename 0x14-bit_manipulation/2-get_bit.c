ac70075e3ee53e60fafb6f242127e85827e0007d
hingamakena <hingamakena@gmail.com>
2024-02-09 07:01:24 -0800

    gets the bit at the given index of the given number

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit of a number
+ * @n: the number
+ * @index: the index of the bit to return
+ *
+ * Return: the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i = 0;
+
+	return (i);
+}
