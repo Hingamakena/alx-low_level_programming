5658d7a28af88a62e3aed3aa5c29f648db21fb32
hingamakena <hingamakena@gmail.com>
2024-02-09 06:50:25 -0800

    goess through the string

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: string to be converted
+ *
+ * Return: return converted int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
