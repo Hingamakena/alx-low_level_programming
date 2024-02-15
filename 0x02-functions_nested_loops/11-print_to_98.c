ea083d0651476072fdac6d0018eb1f1547250aba
hingamakena <hingamakena@gmail.com>
2024-02-15 06:37:56 -0800

    prints to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints to 98
+ * @n: value of n
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n > 98)
+	{
+		printf("%d", n);
+
+		if (n > 97)
+			printf(",");
+
+		n--;
+	}
+	printf("\n");
+
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+
+	while (n < 98)
+	{
+		printf("%d", n);
+
+		if (n < 98)
+			printf(",");
+
+		n++;
+	}
+	printf("\n");
+}
