5c419f7eef38f509c1de87d2df08b594498b65ea
hingamakena <hingamakena@gmail.com>
2024-03-02 05:11:39 -0800

    added a NULL check code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,9 @@ char *str_concat(char *s1, char *s2)
 	s12 = malloc(sizeof(char) * (_strlen(s1) + 1) +
 			sizeof(char) * (_strlen(s2) + 1));
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
 	while (s1[i] != '\0')
 	{
 		s12[i] = s1[j];
