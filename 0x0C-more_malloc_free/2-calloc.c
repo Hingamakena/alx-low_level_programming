54d7e55a52e7cfd72e7cc93d859f73c32bdfecec
hingamakena <hingamakena@gmail.com>
2024-03-04 06:12:57 -0800

    alloc size of int mul by size of array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *space = NULL;
 
-	space = malloc(sizeof(size) * nmemb);
+	space = malloc(sizeof(int) * size);
 
 	if (space == NULL)
 		return (NULL);
