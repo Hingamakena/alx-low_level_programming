e99bc74553e7c9debb48b9a0fb7210705d9330dd
hingamakena <hingamakena@gmail.com>
2024-03-31 09:21:24 -0700

    prints the linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	
+
 	if (h == NULL)
 		return (0);
 
 	while (h != NULL)
 	{
-		printf("[%d] %s\n",h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
 	}
