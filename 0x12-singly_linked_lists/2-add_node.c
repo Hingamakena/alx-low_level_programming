bf08abf055eec22ec4948b06da8b38493144cef9
hingamakena <hingamakena@gmail.com>
2024-03-31 15:36:29 -0700

    adds a node at the beginning of the linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,25 +10,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	int i = 0;
 
 	new = malloc(sizeof(list_t));
-
-	if (head == NULL)
-		return (NULL);
 
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	new->len = i;
 	new->str = strdup(str);
-	(*new).next = *head;
-
-	printf("[%d] %s\n", new->len, new->str);
+	new->next = *head;
+	new->len = strlen(str);
+	*head = new;
 
 	return (new);
 }
