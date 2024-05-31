22f07cee1ffdcb527b3d7c51404b707b0a25ffd8
hingamakena <hingamakena@gmail.com>
2024-05-31 13:15:57 -0700

    prints natureal numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
  */
 void print_to_98(int n)
 {
-	while (n > 98)
+	while (n > 97)
 	{
 		printf("%d", n);
 
@@ -24,7 +24,7 @@ void print_to_98(int n)
 		printf("%d", n);
 	}
 
-	while (n < 98)
+	while (n < 99)
 	{
 		printf("%d", n);
 
